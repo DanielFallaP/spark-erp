d801cff90508f3040cabb1c51dc38786f81115cb
Daniel Falla <danielfap@hotmail.com>
2014-02-12 09:11:42 -0500

    Atributo de la clase

--- Cybersoft/src/co/com/cybersoft/Cyber.java
+++ Cybersoft/src/co/com/cybersoft/Cyber.java
@@ -1,5 +1,15 @@
 package co.com.cybersoft;
 
 public class Cyber {
+  private String name;
 
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+  
+  
 }
