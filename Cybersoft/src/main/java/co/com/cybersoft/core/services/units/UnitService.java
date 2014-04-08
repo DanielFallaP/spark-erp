7ca051819292485a87f1a9cbc6523aeb56d003f4
Daniel Falla <danielfap@hotmail.com>
2014-04-08 18:06:41 -0500

    Eliminación de clases

--- Cybersoft/src/main/java/co/com/cybersoft/core/services/units/UnitService.java
+++
@@ -1,8 +0,0 @@
-package co.com.cybersoft.core.services.units;
-
-import co.com.cybersoft.events.units.RequestUnitsEvent;
-import co.com.cybersoft.events.units.UnitsEvent;
-
-public interface UnitService {
-	UnitsEvent requestUnits(RequestUnitsEvent event);
-}
