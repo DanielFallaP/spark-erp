4ed1ecf2f8b918c3ede380906137694169ddd354
Daniel Falla <danielfap@hotmail.com>
2014-04-08 18:04:00 -0500

    Se eliminan clases de unidad de medida para probar el generador de código

--- Cybersoft/src/main/java/co/com/cybersoft/persistence/services/units/UnitPersistenceService.java
+++
@@ -1,8 +0,0 @@
-package co.com.cybersoft.persistence.services.units;
-
-import co.com.cybersoft.events.units.RequestUnitsEvent;
-import co.com.cybersoft.events.units.UnitsEvent;
-
-public interface UnitPersistenceService {
-	UnitsEvent requestUnits(RequestUnitsEvent event);
-}
