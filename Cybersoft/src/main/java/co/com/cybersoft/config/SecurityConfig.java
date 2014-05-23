d978f37a8214f111c8faf04ae4087d2b1bbecc85
Daniel Falla <danielfap@hotmail.com>
2014-05-23 13:17:44 -0500

    Actualización del generador por cambio de versiones en bibliotecas.

--- Cybersoft/src/main/java/co/com/cybersoft/config/SecurityConfig.java
+++ Cybersoft/src/main/java/co/com/cybersoft/config/SecurityConfig.java
@@ -1,31 +1,31 @@
 package co.com.cybersoft.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
-@EnableWebSecurity
+@EnableWebMvcSecurity
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
-	@Override
-	protected void registerAuthentication(AuthenticationManagerBuilder auth) throws Exception{
-		auth.inMemoryAuthentication().withUser("raul").password("raul").roles("USER");
-		auth.inMemoryAuthentication().withUser("daniel").password("daniel").roles("USER");
-		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("USER");
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication().withUser("raul").password("raul").roles("USER").and()		
+		.withUser("daniel").password("daniel").roles("USER").and()
+		.withUser("admin").password("admin").roles("USER");
 //		LdapAuthenticationProviderConfigurer<AuthenticationManagerBuilder> ldapAuthentication = auth.ldapAuthentication();
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
-		http
-        .authorizeUrls()
+		http.authorizeRequests()
             .anyRequest().hasRole("USER")
             .and()
        .formLogin()
@@ -34,7 +34,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
        .sessionManagement()
             .maximumSessions(1)
             .expiredUrl("/login");
-
+		
+		//FIXME
+		http.csrf().disable();
+		
 	}
 	
 	@Bean
