bb64ff59c69d2d30e047a9897df670b72d89d9c1
Daniel Falla <danielfap@hotmail.com>
2014-06-27 11:51:04 -0500

    Eliminación de tablas del repositorio.

--- Cybersoft/src/main/java/co/com/cybersoft/persistence/repository/items/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package co.com.cybersoft.persistence.repository.items;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import co.com.cybersoft.persistence.domain.Item;
-
-public interface ItemRepository extends MongoRepository<Item, String>{
-	Item findByCode(String code);
-}
