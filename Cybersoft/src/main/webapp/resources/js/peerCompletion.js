3e91d0f63008a8982329c204a2be98b36660d404
Daniel Falla <danielfap@hotmail.com>
2014-07-28 09:33:53 -0500

    Script para peer completion de campos.

--- Cybersoft/src/main/webapp/resources/js/peerCompletion.js
+++ Cybersoft/src/main/webapp/resources/js/peerCompletion.js
@@ -1,4 +1,4 @@
-function peerCompletion(field, peerField, method, table, fieldName){
+function peerCompletion(field, peerField, method, table, fieldName, otherFields){
 							var prefix="";
 				    	    		var path=window.location.pathname.split("/");
 				    	    		if (path[1].localeCompare("configuration")!=0){
@@ -11,5 +11,15 @@ function peerCompletion(field, peerField, method, table, fieldName){
 					    	           }
 					    	      });
 			    	    		
-			    	    		
+			    	    		if (otherFields!=null){
+			    	    			var i;
+			    	    			for (i = 0; i < otherFields.length; ++i) {
+			    	    				$.get(prefix+"/configuration/"+table+"/getListBy"+fieldName+"Return"+otherFields[i]+"?"+fieldName+"="+field.item.value, {}, function (data) {
+							    	           var array=data.split("/////");
+							    	           if (data!=""){
+							    	        	   $("#inputMeasurementUnit").val(array[0]);
+							    	           }
+							    	      });
+			    	    			}
+			    	    		}
 }
