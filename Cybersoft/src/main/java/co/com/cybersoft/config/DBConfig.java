dbd46a1bf816bc16821c86272186eb8a764d6a24
Daniel Falla <danielfalla@cybersystems.com.co>
2015-07-09 21:44:10 -0500

    Persistence config.

--- Cybersoft/src/main/java/co/com/cybersoft/config/DBConfig.java
+++ Cybersoft/src/main/java/co/com/cybersoft/config/DBConfig.java
@@ -3,6 +3,46 @@ package co.com.cybersoft.config;
 public class DBConfig {
 
 	private String address;
+	
+	private String username;
+	
+	private String password;
+	
+	private String driverClass;
+	
+	private String dialect;
+	
+	public String getDialect() {
+		return dialect;
+	}
+
+	public void setDialect(String dialect) {
+		this.dialect = dialect;
+	}
+
+	public String getDriverClass() {
+		return driverClass;
+	}
+
+	public void setDriverClass(String driverClass) {
+		this.driverClass = driverClass;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public String getAddress() {
 		return address;
--- Cybersoft/src/main/java/co/com/cybersoft/config/PersistenceConfig.java
+++ Cybersoft/src/main/java/co/com/cybersoft/config/PersistenceConfig.java
@@ -3,35 +3,26 @@ package co.com.cybersoft.config;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.sql.SQLException;
 import java.util.Properties;
 
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.codehaus.jackson.map.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import co.com.cybersoft.purchase.tables.persistence.repository.continent.ContinentCustomRepo;
-import co.com.cybersoft.purchase.tables.persistence.repository.continent.ContinentCustomRepoImpl;
 import co.com.cybersoft.purchase.tables.persistence.repository.continent.ContinentRepository;
 import co.com.cybersoft.purchase.tables.persistence.repository.region.RegionRepository;
-import co.com.cybersoft.purchase.tables.persistence.services.continent.ContinentPersistenceService;
-import co.com.cybersoft.purchase.tables.persistence.services.continent.ContinentPersistenceServiceImpl;
 
 
 @Configuration
@@ -52,28 +43,15 @@ public class PersistenceConfig {
 		
 		return em;
 	} 
-//	@Bean
-//	  public EntityManagerFactory entityManagerFactory() {
-//
-//	    HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-//	    vendorAdapter.setGenerateDdl(true);
-//
-//	    LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-//	    factory.setJpaVendorAdapter(vendorAdapter);
-//	    factory.setPackagesToScan("co.com.cybersoft");
-//	    factory.setDataSource(dataSource());
-//	    factory.afterPropertiesSet();
-//
-//	    return factory.getObject();
-//	  }
 	
 	@Bean
     public DataSource dataSource(){
+		DBConfig dbConfig = getDBConfig();
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
-		dataSource.setUsername("SYSTEM");
-		dataSource.setPassword("8thisbu6");
-		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+		dataSource.setDriverClassName(dbConfig.getDriverClass());
+		dataSource.setUsername(dbConfig.getUsername());
+		dataSource.setPassword(dbConfig.getPassword());
+		dataSource.setUrl(dbConfig.getAddress());
 		return dataSource;
     }
 
@@ -82,12 +60,12 @@ public class PersistenceConfig {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         return jdbcTemplate;
     }
-	private String getDBAddress(){
+	private DBConfig getDBConfig(){
 		InputStream stream = getClass().getClassLoader().getResourceAsStream("db.json");
 		ObjectMapper mapper = new ObjectMapper();
 		try {
 			DBConfig dbConfig = (DBConfig) mapper.readValue(new InputStreamReader(stream, "UTF8"), DBConfig.class);
-			return dbConfig.getAddress();
+			return dbConfig;
 		} catch (Exception e) {
 			e.printStackTrace();
 		} 
@@ -97,9 +75,11 @@ public class PersistenceConfig {
 
 	
 	Properties additionalProperties() {
+		  DBConfig dbConfig = getDBConfig();
+
 	      Properties properties = new Properties();
 	      properties.setProperty("hibernate.hbm2ddl.auto", "update");
-	      properties.setProperty("hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect");
+	      properties.setProperty("hibernate.dialect", dbConfig.getDialect());
 	      return properties;
    }
 	
