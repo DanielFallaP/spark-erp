c36740a4de888fd5d2be85241baa6c864a61773d
Daniel Falla <danielfalla@cybersystems.com.co>
2015-08-31 22:38:26 -0500

    Excel reporting service response

---
+++ Cybersoft/src/main/java/co/com/cybersoft/man/ExcelReportResponse.java
@@ -0,0 +1,15 @@
+package co.com.cybersoft.man;
+
+public class ExcelReportResponse {
+
+	private String url;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+}
